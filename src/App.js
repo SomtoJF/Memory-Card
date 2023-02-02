0ed5ac0e8c4f8d03353feed2b11e26d159b8b3a5
Somtochukwu Francis <somtofrancis5@gmail.com>
2023-02-02 14:13:12 +0100

    Implement localStorage to store highscore

--- src/App.js
+++ src/App.js
@@ -1,33 +1,43 @@
 import "./App.sass";
 import Nav from "./Components/Nav";
 import Main from "./Components/Main";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import storageAvailable from "./Web-Storage/web-storage";
 
 function App() {
-  const [score, setScore] = useState(0);
-  const [highScore, setHighScore] = useState(0);
+	const [score, setScore] = useState(0);
+	const [highScore, setHighScore] = useState(0);
 
-  const incrementScore = () => {
+	const incrementScore = () => {
 		setScore(score + 1);
 	};
 
 	const incrementHighScore = () => {
-		if(score >= highScore){
-      setHighScore(highScore + 1)
-    }
+		if (score >= highScore) {
+			setHighScore(highScore + 1);
+			localStorage.setItem("high-score", JSON.stringify(score + 1));
+		}
 	};
+
+	useEffect(() => {
+		const storedHighScore =
+			storageAvailable("localStorage") && localStorage.getItem("high-score")
+				? JSON.parse(localStorage.getItem("high-score"))
+				: 0;
+		setHighScore(storedHighScore);
+	}, []);
 
 	const clearScore = () => {
 		setScore(0);
 	};
 
-  return (
+	return (
 		<div className="App">
 			<Nav score={score} highScore={highScore} />
 			<Main
 				incrementHighScore={incrementHighScore}
 				incrementScore={incrementScore}
-        clearScore={clearScore}
+				clearScore={clearScore}
 			/>
 		</div>
 	);
